0efddf6140c5ee1c746a1f32ab5c26b1027e5d85
gaoCalderon <gao.vang86@gmail.com>
2016-08-17 17:27:27 -0500

    worked on Weather

--- Lemonade Stand Project/Lemonade Stand Project/Weather.cs
+++ Lemonade Stand Project/Lemonade Stand Project/Weather.cs
@@ -9,28 +9,64 @@ namespace Lemonade_Stand_Project
     class Weather
     {
         public int temperature;
-        public string forecast;
+        public int forecast;
 
-        public Weather(int temperature, string forecast)
+        public Weather(int temperature, int forecast)
         {
             this.temperature = temperature;
             this.forecast = forecast;
         }
 
-        List<string> typeOfForecast = new List<string>() { "Sunny", "Partly Cloudy", "Partly Cloudy with Rain", "Cloudy", "Cloudy with Rain", "Rainy" };
+        //List<string> typeOfForecast = new List<string>() { "Sunny", "Partly Cloudy", "Partly Cloudy with Rain", "Cloudy", "Cloudy with Rain", "Rainy" };
 
-        public string getForecast()
+        public int getForecast()
         {
-            this.forecast = randomForecast();
+            this.forecast = setRandomForecast();
             return this.forecast;
         }
 
-        public string randomForecast()
+        public int setRandomForecast()
         {
             Random f = new Random();
-            f.Next(0, typeOfForecast.Count);
-            string choice = typeOfForecast[f.Next(0, typeOfForecast.Count)];
-            return choice;
+            //f.Next(0, typeOfForecast.Count);
+            //string choice = typeOfForecast[f.Next(0, typeOfForecast.Count)];
+            //return choice;
+            int setRanForecast = f.Next(1, 6);
+            return setRanForecast;
+        }
+
+        public int forecastPrintAffecter()
+        {
+            int results = setRandomForecast();
+            return results;
+            if (results == 1)
+            {
+                Console.WriteLine("Weather Forecast: Sunny");
+            }
+            else if (results == 2)
+            {
+                Console.WriteLine("Weather Forcast: Partly Cloudy");
+            }
+            else if (results == 3)
+            {
+                Console.WriteLine("Weather Forcast: Partly Overcast with Light Rain");
+            }
+            else if (results == 4)
+            {
+                Console.WriteLine("Weather Forcast: All Day Overcast");
+            }
+            else if (results == 5)
+            {
+                Console.WriteLine("Weather Forcast: Rainy");
+            }
+            else if (results == 6)
+            {
+                Console.WriteLine("Weather Forecast: Heavy Rains");
+            }
+            else
+            {
+                forecastPrintAffecter();
+            }
         }
 
         public int setRandomTemperature()
@@ -43,13 +79,60 @@ namespace Lemonade_Stand_Project
         public int GetDayTemperature()
         {
             this.temperature = setRandomTemperature();
-            return temperature;
+            return this.temperature;
         }
 
-        public void weatherReport()
+        public void temperatureCall()
         {
-            Console.WriteLine("Weather report {0} at about {1} degrees Fahrenheit.", getForecast(), GetDayTemperature());
+            Console.WriteLine("Temperature in the hight: {0}F", GetDayTemperature());
         }
+
+        public int temperaturePrintAffecter()
+        {
+            int results = setRandomTemperature();
+            return results;
+            if (results <= 72)
+            {
+                temperatureCall();
+            }
+            else if (results <= 79)
+            {
+                temperatureCall();
+            }
+            else if (results <= 86)
+            {
+                temperatureCall();
+            }
+            else if (results <= 93)
+            {
+                temperatureCall();
+            }
+            else if (results <= 100)
+            {
+                temperatureCall();
+            }
+            else if (results <= 107)
+            {
+                temperatureCall();
+            }
+            else
+            {
+                temperaturePrintAffecter();
+            }
+        }
+
+
+       // public void weatherForecast()
+        //{
+           // Console.WriteLine("Weather Forcast:");
+           // Console.WriteLine("Predicts: {0}", setRandomForecast());
+            //Console.WriteLine("in the highs of {0} degrees Fahrenheit", setRandomTemperature());
+        //}
+
+        //public void weatherReport()
+        //{
+            //Console.WriteLine("Weather report {0} at about {1} degrees Fahrenheit.", getForecast(), GetDayTemperature());
+        //}
 
     }
 
